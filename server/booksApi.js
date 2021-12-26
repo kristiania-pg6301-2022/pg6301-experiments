3f966ab442494267e80c6aa4187c498743124685
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-12-27 00:59:15 +0100

    Store library in MongoDB (breaks tests)

--- server/booksApi.js
+++ server/booksApi.js
@@ -1,22 +1,38 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+const { ATLAS_URL } = process.env;
+const client = new MongoClient(ATLAS_URL, {
+    useNewUrlParser: true, useUnifiedTopology: true
+});
 
 const router = express.Router();
 
-const books = [
-    {
-        id: 1, author: "Johannes", title: "Some title", year: 2020
-    }
-];
+client.connect().then(conn => {
+    const db = conn.db("library");
 
-router.get("", (req, res) => {
-    res.json(books).end();
+    router.get("", (req, res) => {
+        db.collection("books")
+            .find({})
+            .map(({_id, title, author, year}) => ({id: _id, title, author, year}))
+            .toArray()
+            .then(result => res.json(result).end());
+    })
+
+    router.post("", async(req, res) => {
+        const {title, author, year} = req.body;
+
+        await db.collection("books").insertOne({
+            title, author, year
+        });
+
+        res.status(200).end();
+    })
+
 })
 
 
-router.post("", async(req, res) => {
-    const {title, author, year} = req.body;
-    books.push({title, author, year, id: books.length});
-    res.status(200).end();
-})
+
+
+
 
 module.exports = router;

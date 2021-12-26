d19fb47a82cc94a1cb849c37f71af0aebd32f756
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-12-26 14:41:03 +0100

    Style and structure

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,11 @@ import * as ReactDOM from "react-dom";
 import * as React from "react";
 import {Application} from "./src/application";
 
-ReactDOM.render(<Application/>, document.getElementById("app"));
+import "./style.css";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Application/>
+    </React.StrictMode>,
+    document.getElementById("app"))
+;
--- client/src/application.jsx
+++ client/src/application.jsx
@@ -1,5 +1,32 @@
 import * as React from "react";
 
-export function Application() {
-    return <h1>Kristiania Library Application</h1>;
+function ApplicationHeader() {
+    return <header><h1>Kristiania Library</h1></header>;
 }
+
+function ApplicationMenu() {
+    return <nav>
+        <ul>
+            <li>View books</li>
+            <li>View authors</li>
+            <li>Add book</li>
+        </ul>
+    </nav>;
+}
+
+function ApplicationMain() {
+    return <main>
+        <div>
+            Welcome
+        </div>
+    </main>;
+}
+
+export function Application() {
+    return <main>
+        <ApplicationHeader />
+        <ApplicationMenu />
+        <ApplicationMain />
+    </main>
+}
+

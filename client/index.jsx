1f0e6136362989df7e884a6e31e5c9c9504aa218
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-12-26 14:22:18 +0100

    Extracted client/src/application.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,5 @@
 import * as ReactDOM from "react-dom";
 import * as React from "react";
+import {Application} from "./src/application";
 
-ReactDOM.render(<h1>React App</h1>, document.getElementById("app"));
+ReactDOM.render(<Application/>, document.getElementById("app"));

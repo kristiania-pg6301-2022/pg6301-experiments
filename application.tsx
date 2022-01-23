0d9cc5c577c4aa1b89ede00021f233158e87eea9
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-23 20:54:53 +0100

    Converted to typescript

---
+++ application.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+
+export function Application() {
+    const someone: string = "React";
+    return <h1>Hello {someone}</h1>;
+}
---
+++ index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import {Application} from "./application";
+
+ReactDOM.render(<Application />, document.getElementById("app"));
